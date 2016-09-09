96f4effc39ffa20b9ff9e4ec21feac289de79879
Jwookie55 <jwookie55@live.com>
2016-09-09 15:23:01 -0700

    created userfactory and finished eventfactory and userprofilecontroller

--- client/app.js
+++ client/app.js
@@ -6,6 +6,10 @@ App.config(configFunction);
 function configFunction($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
+      templateUrl: './partials/login.html',
+      controller: 'LoginController',
+    })
+    .when('/profile', {
       templateUrl: './partials/userprofile.html',
       controller: 'UserProfileController',
     })
--- client/factories/eventFactory.js
+++ client/factories/eventFactory.js
@@ -1,15 +1,23 @@
 angular
   .module('EventFactory', [])
-  .controller('EventFactory', eventFActory)
+  .controller('EventFactory', eventFactory)
 
 function eventFactory($http) {
   let event = '';
+  let user = '';
 
   event.updateEvent = function (data) {
     event = data;
   };
+  event.updateUser = function (data) {
+    user = data;
+  };
 
-  event.fetch = function () {
+  event.fetchUsers = function () {
     return $http.get(ADDURL + '?activity=' + event)
+  };
+
+  event.fetchActivities = function (data) {
+    return $http.get(ADDURL + '?search=' + data)
   };
 }
