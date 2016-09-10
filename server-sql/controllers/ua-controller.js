d145b698dcf2b56059fecf538c1f24539103d279
Jwookie55 <jwookie55@live.com>
2016-09-10 13:47:24 -0700

    finished auto add to newly created activity

--- server-sql/controllers/ua-controller.js
+++ server-sql/controllers/ua-controller.js
@@ -1,25 +1,24 @@
 "use strict";
-
 const database = require('../models/database');
+
 const sequelize = database.sequelize;
 const UserActivity = database.UserActivity;
 const Activity = database.Activity;
 
-function index(req, res) {
-	// var options = {};
-	// options.where = req.query;
-	//displays all activities associated with users?
-	UserActivity.findAll({}).then(function(uas) {
-  	var indexArr = uas.map(function(e){return e.dataValues})
-  	// console.log(uas);
-  	res.json(uas);
-	});
+function index(req, res) { // displays all activities associated with users? for devs not production
+  UserActivity.findAll({}).then((uas) => {
+    res.json(uas);
+  });
 }
 
-function add(req, res, next) {
-	// console.log(req.body);
-	
-	UserActivity.create(req.body.data[0], err => {
+function add(req, res, next) { // associates a user and a activity
+  if (req.actKey) {
+    const updateObj = { "activityid": req.actKey, "userid": req.body.data[0].userid }
+    UserActivity.create(req.body.data[0], err => {
+      if (err) console.error(err);
+  });
+  }
+  UserActivity.create(req.body.data[0], err => {
     if (err) console.error(err);
   });
   next();
@@ -30,21 +29,19 @@ function show(req, res, next) {
 	// Activity.create(req.body[0], err => {
   //   if (err) console.error(err);
   // });
-	
   next();
 }
 
-function findbyact (req, res, next){
-	Activity.sequelize.query('SELECT "username" from "users" join "useractivities" on ("useractivities"."userId" = "users"."_id") join "activities" on ("activities"."_id" = "useractivities"."activityId") where "actname" =\'' + req.params.actname + '\'').then(function(data){
-      let output = {users: data[0]};
-			console.log(output);
-		
-      res.json(output);
-			next();			
-			
-    })
-
+function findbyact(req, res, next) { // finds all users by activity
+  Activity.sequelize.query('SELECT "username" from "users" join "useractivities" on ' +
+  '("useractivities"."userId" = "users"."_id") join "activities" on ("activities"."_id" = ' +
+  '"useractivities"."activityId") where "actname" =\'' + req.params.actname + '\'')
+  .then((data) => {
+    const output = { users: data[0] };
+    res.json(output);
+    next();
+  });
 }
 
 
-module.exports = { index, add, show, findbyact};
+module.exports = { index, add, show, findbyact };
--- server-sql/models/database.js
+++ server-sql/models/database.js
@@ -1,34 +1,37 @@
+'use strict';
+
 const Sequelize = require('sequelize');
 
-//if there is a password, make it the third parameter after the username; otherwise, make it null and include a comma after it
+// if there is a password, make it the third parameter after the username,
+// otherwise, make it null and include a comma after it
 const sequelize = new Sequelize('horegyuy', 'horegyuy', 'YDEWOzNuQL9R8hViSAvrZD9KIRxmMOYC', {
   host: 'elmer-01.db.elephantsql.com',
-  dialect: 'postgres'
+  dialect: 'postgres',
 });
 
-var User = sequelize.define('users', {
-	_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement:true},
-	username: Sequelize.STRING,
-	profilepic: Sequelize.STRING,
+const User = sequelize.define('users', {
+  _id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  username: Sequelize.STRING,
+  profilepic: Sequelize.STRING,
   bio: Sequelize.STRING,
-  password:Sequelize.STRING,
+  password: Sequelize.STRING,
 }, {
-  timestamp: false
+  timestamp: false,
 });
 
-var Activity = sequelize.define('activities', {
-	_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement:true},
-	actname: Sequelize.STRING,
-	actdesc: Sequelize.STRING,
+const Activity = sequelize.define('activities', {
+  _id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  actname: Sequelize.STRING,
+  actdesc: Sequelize.STRING,
 });
 
-var UserActivity = sequelize.define('useractivities', {
-  _id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement:true },
+const UserActivity = sequelize.define('useractivities', {
+  _id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   status: Sequelize.BOOLEAN, // if open, true, if completed false
 });
 
-Activity.belongsToMany(User, { through: 'useractivities'});
-User.belongsToMany(Activity, { through: 'useractivities'});
+Activity.belongsToMany(User, { through: 'useractivities' });
+User.belongsToMany(Activity, { through: 'useractivities' });
 
 // Sync all models that aren't already in the database
 sequelize.sync()
@@ -37,11 +40,12 @@ sequelize.sync()
 // // Drop all tables -- ran once 9/9 2:48pm
 // sequelize.drop()
 // emit handling:
-.then(function() {
+.then(() => {
   // woot woot
-}).catch(function(error) {
-  // whooops
-})
+}).catch((error) => {
+  console.log(error);
+});
 
-	module.exports = {sequelize: sequelize, User: User, Activity:Activity, UserActivity:UserActivity}
+module.exports = { sequelize, User, Activity, UserActivity }
+
 
