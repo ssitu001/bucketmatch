44bddbbe57093182bf07a1b6afb8d56d59e2a375
sneakykate <sneakyk8@gmail.com>
2016-09-10 10:44:48 -0700

    add route for find users by activity

--- server-sql/controllers/ua-controller.js
+++ server-sql/controllers/ua-controller.js
@@ -1,6 +1,7 @@
 const database = require('../models/database');
 const sequelize = database.sequelize;
 const UserActivity = database.UserActivity;
+const Activity = database.Activity;
 
 function index(req, res) {
 	// var options = {};
@@ -30,5 +31,17 @@ function show(req, res, next) {
   next();
 }
 
+function findbyact (req, res, next){
+	Activity.sequelize.query('SELECT "username" from "users" join "useractivities" on ("useractivities"."userId" = "users"."_id") join "activities" on ("activities"."_id" = "useractivities"."activityId") where "actname" =\'' + req.params.actname + '\'').then(function(data){
+      let output = {users: data[0]};
+			console.log(output);
+		
+      res.json(output);
+			next();			
+			
+    })
 
-module.exports = { index, add, show};
+}
+
+
+module.exports = { index, add, show, findbyact};
--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
 })
 
-app.get('/user/:username/:password', userCtrl.show, userCtrl.conn); //to get a single user's profile'
-// app.get('/someshit/', userCtrl.conn); //to get a single user's profile'
+app.get('/user/:username/:password', userCtrl.show, userCtrl.conn); //to log in and get current user info
+app.get('/userinfo/:username/', userCtrl.profile, (req, res) => { res.end() }); //to get a single user's profile with limited info'
+
 
 app.get('/test', userCtrl.index); //full list of users, not needed for front-end
 app.post('/user/add', userCtrl.add, (req, res) => { res.end() });//to add a single user
@@ -25,7 +26,7 @@ app.get('/useractivities', uaCtrl.index, (req, res) => { res.end() });//to view 
 app.post('/useractivity/add', uaCtrl.add, (req, res) => { res.end() });//to add a new activity TO a User
 // app.put('/useractivity/close', uaCtrl.close, (req, res) => {res.end() }); // to mark activity as done
 
-app.get('/activity/', uaCtrl.add, (req, res) => { res.end() });//to add a new activity TO a User
+app.get('/useractivity/findbyact/:actname', uaCtrl.findbyact, (req, res) => { res.end() });//to find all users by activity
 
 
 app.use(express.static(path.join(__dirname + '/client/')));
